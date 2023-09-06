f987c3137b46df3e5912b67dd2d343a9e8ab1bed
SiHecate <blrumutcan64@gmail.com>
2023-09-06 15:18:45 +0300

    Değişiklikler işlendi

--- Model/cryptoWallet.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type CryptoWallet struct {
-	ID               uint    `json:"cryptoID" gorm:"primaryKey"`
-	cryptoName       string  `json:"cryptowalletCryptoName"`
-	cryptoTotalPrice float64 `json:"cryptowalletCryptoTotalPrice"`
-	// Her crypto için ayrı bir yer tutulması gerekiyor
-}
