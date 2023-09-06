2a6ccadcb56e162a0ca9c02ba584ac29110b98bb
SiHecate <blrumutcan64@gmail.com>
2023-09-06 16:50:35 +0300

    Değişiklikler eklendi

--- Main.go
+++ Main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"gokripto/Database"
-<<<<<<< HEAD
-=======
-	"gokripto/controllers"
->>>>>>> origin/main
 	"gokripto/routes"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,26 +10,10 @@ import (
 
 func main() {
 	Database.Connect()
-<<<<<<< HEAD
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 	}))
 	routes.Setup(app)
-=======
-
-	app := fiber.New()
-
-	app.Use(cors.New(cors.Config{
-		AllowCredentials: true,
-	}))
-
-	routes.Setup(app)
-	app.Static("/", "./static") // Bu satır, statik dosyaların sunulacağı klasörü belirtir
-	app.Get("/get-exchange-rate", controllers.AddCryptoData)
-	app.Get("/update-exchange-rate", controllers.UpdateCryptoData)
-	app.Get("/ShowAll", controllers.ListAllCryptos)
-
->>>>>>> origin/main
 	app.Listen(":3000")
 }
