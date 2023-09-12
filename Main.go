ac117f38f9849864cbc15e5b39d659cc4c5ea013
SiHecate <blrumutcan64@gmail.com>
2023-09-12 17:34:34 +0300

    Değişiklikler

--- Main.go
+++ Main.go
@@ -4,6 +4,7 @@ import (
 	"gokripto/Database"
 	websocket "gokripto/Websocket"
 	"gokripto/routes"
+	"sync"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -16,6 +17,13 @@ func main() {
 		AllowCredentials: true,
 	}))
 	routes.Setup(app)
-	go websocket.StartWebSocket(app)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		websocket.StartWebSocket(app)
+		wg.Done()
+	}()
+	wg.Wait()
 	app.Listen(":3000")
 }
