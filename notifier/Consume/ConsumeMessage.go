3e59c7fa4470d7cae310bdb78742867470746b67
SiHecate <blrumutcan64@gmail.com>
2023-10-17 00:36:41 +0300

    Table oluşturmayı microservice'e aktarma

--- notifier/Consume/ConsumeMessage.go
+++ notifier/Consume/ConsumeMessage.go
@@ -1,6 +1,8 @@
 package consume
 
 import (
+	"Notifier/database"
+	"Notifier/model"
 	"encoding/json"
 	"fmt"
 	"time"
@@ -23,25 +25,51 @@ func ConsumeMessages() {
 
 	run := true
 
-	type message struct {
-		Mail string `json:"mail"`
-		Code string `json:"code"`
-	}
-
 	for run {
 		msg, err := c.ReadMessage(time.Second)
 		if err == nil {
 			// Mesajın içeriğini JSON olarak parse edin
-			var messageData message
+			var messageData Message
 			err := json.Unmarshal(msg.Value, &messageData)
 			if err != nil {
 				fmt.Printf("Mesaj JSON çözümlemesi hatası: %v\n", err)
 			} else {
 				// "Mail" ve "Code" alanlarını alın ve yazdırın
 				fmt.Printf("Mail: %s, Code: %s\n", messageData.Mail, messageData.Code)
+				VerificationTable()
 			}
 		}
 	}
 
 	c.Close()
+}
+
+type Message struct {
+	Mail string `json:"mail"`
+	Code string `json:"code"`
+}
+
+func ConvertMessage(message *kafka.Message) *Message {
+	var messageData Message
+	err := json.Unmarshal(message.Value, &messageData)
+	if err != nil {
+		return nil
+	}
+
+	return &messageData
+}
+
+func VerificationTable() error {
+	messageData := Message{}
+
+	verfication := model.Verfication{
+		Email:       messageData.Mail,
+		Verfication: messageData.Code,
+	}
+
+	if err := database.Conn.Create(&verfication).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
